44390557bf68a56757ac4476804a5e09f6e1c5d7
Madson Jr <madson.verdi@softpampa.com>
2016-05-11 11:25:00 -0300

    Criação e atualização de arquivos na VM

--- easymed/puphpet/shell/initial-setup.sh
+++ easymed/puphpet/shell/initial-setup.sh
@@ -76,9 +76,35 @@ if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
         echo 'Finished installing basic curl packages'
     fi
 
+    echo 'Installing Node.js'
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+    apt-get install -y nodejs >/dev/null
+    echo 'Finished installing Node.js'
+
+    echo 'Installing Bower'
+    npm install -g bower
+    echo 'Finished installing Bower'
+
+    echo 'Installing Grunt-cli'
+    npm install -g grunt-cli
+    echo 'Finished installing Grunt-cli'
+
     echo 'Installing build-essential packages'
     apt-get -y install build-essential >/dev/null
     echo 'Finished installing build-essential packages'
+
+    echo 'Installing Composer'
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    mv composer.phar /usr/local/bin/composer
+    echo 'Finished installing Composer'
+
+    echo 'Installing php dependencies'
+    apt-get install php5-gd -y >/dev/null
+    echo 'finishing installing php dependencies'
+
 elif [[ "${OS}" == 'centos' ]]; then
     echo 'Adding repos: elrepo, epel, scl'
     perl -p -i -e 's@enabled=1@enabled=0@gi' /etc/yum/pluginconf.d/fastestmirror.conf
